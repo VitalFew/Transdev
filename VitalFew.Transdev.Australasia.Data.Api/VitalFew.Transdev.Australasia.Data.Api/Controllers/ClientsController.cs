b1560915e202dd5b7b21a3417f135ff78f947004
dhanushkademel <dhanushka.demel@gmail.com>
2015-08-04 22:02:18 +1000

    Updating the project

---
+++ VitalFew.Transdev.Australasia.Data.Api/VitalFew.Transdev.Australasia.Data.Api/Controllers/ClientsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VitalFew.Transdev.Australasia.Data.Api.Controllers
+{
+    public class ClientsController : Controller
+    {
+        // GET: Clients
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
