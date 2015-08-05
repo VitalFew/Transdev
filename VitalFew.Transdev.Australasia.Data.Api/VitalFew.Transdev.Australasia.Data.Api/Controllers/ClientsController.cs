600331ece99787e4f0a5e8da2e1c11c539349596
charithFIT <cmadu701@gmail.com>
2015-08-05 22:46:03 +0530

    Add new client and edit exisiting clients

--- VitalFew.Transdev.Australasia.Data.Api/VitalFew.Transdev.Australasia.Data.Api/Controllers/ClientsController.cs
+++ VitalFew.Transdev.Australasia.Data.Api/VitalFew.Transdev.Australasia.Data.Api/Controllers/ClientsController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.Mvc;
 using VitalFew.Transdev.Australasia.Data.Api.Models;
 using VitalFew.Transdev.Australasia.Data.Api.Models.Dto;
@@ -58,6 +60,59 @@ namespace VitalFew.Transdev.Australasia.Data.Api.Controllers
                 iTotalDisplayRecords = displayedClients.Count(),
                 aaData = result
             }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Guid guid)
+        {
+            var client = new ClientProvider().GetAll().Where(x => x.CLIENT_ID.HasValue && x.CLIENT_ID.Value.Equals(guid)).FirstOrDefault();
+
+            return View(client);
+        }
+
+        public async System.Threading.Tasks.Task<ActionResult> Edit(VF_API_CATALOG_CLIENTS client)
+        {
+            await new ClientProvider().Save(client);
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View(new VF_API_CATALOG_CLIENTS { CLIENT_TOKEN= GenerateId()});
+        }
+
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> Add(VF_API_CATALOG_CLIENTS client)
+        {
+            //TODO: Should Generate from SQL
+            client.CLIENT_ID = Guid.NewGuid();
+            await new ClientProvider().Save(client);
+
+            return RedirectToAction("Index");
+        }
+
+        public JsonResult GetToken()
+        {
+            return Json(GenerateId(), JsonRequestBehavior.AllowGet);
+        }
+
+        private string GenerateId()
+        {
+            long i = 1;
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+            {
+                i *= ((int)b + 1);
+            }
+            var key= string.Format("{0:x}", i - DateTime.Now.Ticks);
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, 20-key.Length)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+
+            return key + result;
         }
     }
 }
