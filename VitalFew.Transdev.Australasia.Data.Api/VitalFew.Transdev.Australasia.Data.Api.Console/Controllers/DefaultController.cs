e423720725bcc095f54c78956d1ab151db22a376
dhanushkademel <dhanushka.demel@gmail.com>
2015-08-09 12:55:00 +1000

    Updaating

---
+++ VitalFew.Transdev.Australasia.Data.Api/VitalFew.Transdev.Australasia.Data.Api.Console/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VitalFew.Transdev.Australasia.Data.Api.Console.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ VitalFew.Transdev.Australasia.Data.Api/VitalFew.Transdev.Australasia.Data.Api.Console/Providers/Contract/IClientProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VitalFew.Transdev.Australasia.Data.Core.Database;
+
+namespace VitalFew.Transdev.Australasia.Data.Api.Console.Providers.Contract
+{
+    public interface IClientProvider
+    {
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        IQueryable<VF_API_CATALOG_CLIENTS> GetAll();
+
+        /// <summary>
+        /// Saves the specified client.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        /// <returns></returns>
+        Task<int> Save(VF_API_CATALOG_CLIENTS client);
+    }
+}
