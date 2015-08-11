481d61be10142649643390c647be83b37d198160
dhanushkademel <dhanushka.demel@gmail.com>
2015-08-11 21:46:20 +1000

    Updating

--- VitalFew.Transdev.Australasia.Data.Api/VitalFew.Transdev.Australasia.Data.Api.Console/App_Start/RouteConfig.cs
+++ VitalFew.Transdev.Australasia.Data.Api/VitalFew.Transdev.Australasia.Data.Api.Console/App_Start/RouteConfig.cs
@@ -10,10 +10,17 @@ namespace VitalFew.Transdev.Australasia.Data.Api.Console
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+              name: "ClientObjects",
+              url: "ClientObjects/{id}",
+              defaults: new { controller = "ClientObjects", action = "Index", id = UrlParameter.Optional, }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional, }
             );
+            
         }
     }
 }
